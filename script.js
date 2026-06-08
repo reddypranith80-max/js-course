647b5e1dedec63e748468c9b7741f59650972a6e
reddypranith80-max <reddypranith80@gmail.com>
2026-06-08 18:07:27 +0530

    checking the template literals

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-let js = "amazing";
+// let js = "amazing";
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Pranith");
+// console.log("Pranith");
 
-let firstName = "Reddy";
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Reddy";
+// console.log(firstName);
+// console.log(firstName);
 
-let myFirstJob = "Coder";
-let myCurrentJob = "Programmer";
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Programmer";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+const firstName = "Pranith";
+const job = "Programmer";
+const birthYear = 2000;
+const year = 2024;
+
+const pranith =
+  "I'm" +
+  firstName +
+  " a " +
+  job +
+  " born in " +
+  birthYear +
+  " today is " +
+  year;
+console.log(pranith);
+
+const pranithNew = ` I'm ${firstName} a ${year - birthYear} old ${job}!`;
+
+console.log(pranithNew);
+
+console.log(
+  "this is \n\
+    string with \n\
+    multiple lines",
+);
+
+console.log(
+  `this is 
+    string with
+    multiple lines`,
+);
