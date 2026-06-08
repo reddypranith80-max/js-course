bd920588f09263380946021ef52c45073a566618
reddypranith80-max <reddypranith80@gmail.com>
2026-06-09 02:46:25 +0530

    type conversion and coersion

--- script.js
+++ script.js
@@ -47,22 +47,46 @@
 //     multiple lines`,
 // );
 
-const age = 15;
+// const age = 15;
 
-if (age >= 18) {
-  console.log("sarah is of age and can go to apply license 🚗");
-} else {
-  const years = 18 - age;
+// if (age >= 18) {
+//   console.log("sarah is of age and can go to apply license 🚗");
+// } else {
+//   const years = 18 - age;
 
-  console.log(`sarah is still ${years} years young :) `);
-}
+//   console.log(`sarah is still ${years} years young :) `);
+// }
 
-const birth = 2004;
+// const birth = 2004;
 
-let century;
-if (birth <= 2000) {
-  century = 20;
-} else {
-  century = 21;
-}
-console.log(century);
+// let century;
+// if (birth <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
+
+//type conversion and coersion
+
+let a = "Pranith";
+console.log(Number(a)); //return Nan
+
+// console.log(typeof NaN); // returns number
+
+let b = 11;
+console.log(typeof b);
+
+console.log(String(b), b);
+console.log(typeof b); //return number even when converted only the scope is in the console.log in prev command not in the whole val;
+
+const val = "11" + 1;
+
+const ans = val - 1;
+
+console.log(val, ans);
+console.log(typeof val, typeof ans);
+
+// until - we get the number as the type if we use (+) we get it as format of string
+
+console.log("11" - "4" - "3" - 3 + "5");
