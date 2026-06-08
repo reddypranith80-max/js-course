7685dac2c1bbc4874cf0846603d8e064197b27db
reddypranith80-max <reddypranith80@gmail.com>
2026-06-09 02:34:52 +0530

    if else loops

--- script.js
+++ script.js
@@ -15,34 +15,54 @@
 
 // console.log(typeof null);
 
-const firstName = "Pranith";
-const job = "Programmer";
-const birthYear = 2000;
-const year = 2024;
+// const firstName = "Pranith";
+// const job = "Programmer";
+// const birthYear = 2000;
+// const year = 2024;
 
-const pranith =
-  "I'm" +
-  firstName +
-  " a " +
-  job +
-  " born in " +
-  birthYear +
-  " today is " +
-  year;
-console.log(pranith);
+// const pranith =
+//   "I'm" +
+//   firstName +
+//   " a " +
+//   job +
+//   " born in " +
+//   birthYear +
+//   " today is " +
+//   year;
+// console.log(pranith);
 
-const pranithNew = ` I'm ${firstName} a ${year - birthYear} old ${job}!`;
+// const pranithNew = ` I'm ${firstName} a ${year - birthYear} old ${job}!`;
 
-console.log(pranithNew);
+// console.log(pranithNew);
 
-console.log(
-  "this is \n\
-    string with \n\
-    multiple lines",
-);
+// console.log(
+//   "this is \n\
+//     string with \n\
+//     multiple lines",
+// );
 
-console.log(
-  `this is 
-    string with
-    multiple lines`,
-);
+// console.log(
+//   `this is
+//     string with
+//     multiple lines`,
+// );
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("sarah is of age and can go to apply license 🚗");
+} else {
+  const years = 18 - age;
+
+  console.log(`sarah is still ${years} years young :) `);
+}
+
+const birth = 2004;
+
+let century;
+if (birth <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
