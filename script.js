01d5584a33cb5219d5be6a2a777d2a518cb2c929
Darkcrow07 <pranithgurram@gmail.com>
2026-06-08 10:50:20 +0530

    adding components

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('yoo javascript is fun');
+let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Pranith");
+
+let firstName = "Reddy";
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Programmer";
+
+console.log(myFirstJob);
+
+console.log(typeof null);
